f7c700f52663117a9d9b03c510bcc4cc43b48b44
James Cao <jcao5360@gmail.com>
2025-03-26 16:43:32 -0400

    init commit

---
+++ NamedPhotos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NamedPhotos
+//
+//  Created by James Cao on 3/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NamedPhotos/NamedPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamedPhotosApp.swift
+//  NamedPhotos
+//
+//  Created by James Cao on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NamedPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
